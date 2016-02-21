423fae8ec44071e9beebda2ece0ad8861c75e9bc
Wolfgang <glueck.wolfgang@gmail.com>
2016-02-21 22:31:38 +0100

    2016_02_21

--- Robot_Toolbox/Region.py
+++ Robot_Toolbox/Region.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 ###############################################################################
 # Class of Region
-# Version:  2016.02.20
+# Version:  2016.02.21
 #
 # A region is a room or a door
+#
+# For ypur comfort:
+# Use the parameter sequence of Region according to type part sequence
 #
 # dtype       the type of region ("M", "LM", "LMR")
 # xm, ym      the coordinates of the middle point of the rectangle M
@@ -23,19 +26,35 @@
 
 
 class Region:
-    def __init__(self, dtype, xm, ym, wm, hm, anglem, wl= None, hl=None, yl=None, wr=None, hr=None, yr=None):
+    def __init__(self, dtype, anglem, p1=None, p2=None, p3=None, p4=None,
+                        p5= None, p6=None, p7=None,p8=None,p9=None, p10=None):
         self.dtype = dtype
-        self.xM = xm
-        self.yM = ym
-        self.widthM = wm
-        self.heightM = hm
         self.angleM = anglem
-        self.widthL = wl
-        self.heightL = hl
-        self.yL = yl
-        self.widthR = wr
-        self.heightR = hr
-        self.yR = yr
+        # M
+        if dtype == "M":
+            self.xM = p1
+            self.yM = p2
+            self.widthM = p3
+            self.heightM = p4
+
+            self.widthL = p5
+            self.heightL = p6
+            self.yL = p7
+        else:
+            # LM or LMR
+            self.widthL = p1
+            self.heightL = p2
+            self.yL = p3
+
+            self.xM = p4
+            self.yM = p5
+            self.widthM = p6
+            self.heightM = p7
+
+        # M, LM, LMR
+        self.widthR = p8
+        self.heightR = p9
+        self.yR = p10
 
 
     def __str__(self):
