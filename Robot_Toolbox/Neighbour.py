817fdf958bc15b89ce3fe04c0e5ad82bb0687367
Wolfgang <glueck.wolfgang@gmail.com>
2016-02-21 23:33:12 +0100

    2016_02_21

--- Robot_Toolbox/Neighbour.py
+++ Robot_Toolbox/Neighbour.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ###############################################################################
 # Class of Neighbour
-# Version:  2016.02.20
+# Version:  2016.02.21
 #
 # one or more parts of a region ("M", "L", "R") can have up to 4 neighbours
 # a neighbour is docked either on north, south, west or east of the part
@@ -11,13 +11,21 @@
 
 
 class Neighbour:
-    def __init__(self, region, dPart=None, north=None, south=None, west=None, east=None):
+    def __init__(self, region, dPart=None,
+                north=None, dPartNorth=None,
+                south=None, dPartSouth=None,
+                west=None, dPartWest=None,
+                east=None, dPartEast=None):
         self.dPart = dPart
         self.region = region
         self.northN = north
+        self.dPartNorth = dPartNorth
         self.southN = south
+        self.dPartSouth = dPartSouth
         self.westN = west
+        self.dPartWest = dPartWest
         self.eastN = east
+        self.dPartEast = dPartEast
 
     def __str__(self):
         nachricht = "Class neighbour"
